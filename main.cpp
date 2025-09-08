05748de9322a0bc2a54c419c39fb6ac87438e079
Artem <Sorokiny.69@gmail.com>
2025-09-08 23:01:56 +0500

    Added player movement
    Added player spritesheet
    Added movemets

--- main.cpp
+++ main.cpp
@@ -1,46 +1,63 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
     
     //--------------------------- INITIALIZATION ---------------------------
-    // set anti-alias and creates the window
     sf::ContextSettings settings;
-    settings.antiAliasingLevel = 8;
+    settings.antiAliasingLevel = 8; // set anti-alias and creates the window
     sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "My window", sf::Style::Default, sf::State::Windowed, settings);
-
-    sf::CircleShape shape(50.f, 3);
-    shape.setFillColor(sf::Color(100, 250, 50));
-    shape.setPosition(sf::Vector2f(100, 100));
-    shape.setOutlineThickness(10);
-    shape.setOutlineColor(sf::Color::Blue);
-
-    sf::RectangleShape rectangle({ 100.f, 3.f });
-    rectangle.setPosition(sf::Vector2f(500, 100));
-    rectangle.setFillColor(sf::Color::Red);
-    // sets pivot point to the center instead of top left
-    rectangle.setOrigin(rectangle.getSize() / 2.f);
-    rectangle.setRotation(sf::degrees(45));
     //--------------------------- INITIALIZATION ---------------------------
+
      
-    // run the program as long as the window is open
-    while (window.isOpen())
+    //--------------------------- LOAD ---------------------------
+    sf::Texture playerTexture;
+    if (!playerTexture.loadFromFile("D:\\rpg_game\\assets\\player\\textures\\spritesheet.png"))
+    {
+        std::cout << "player image failed to load" << std::endl;
+        return -1;
+    }
+    
+    sf::Sprite playerSprite(playerTexture); // apply the texture to the sprite
+
+    int Xindex = 0;
+    int Yindex = 0;                         // position and width & height
+    playerSprite.setTextureRect(sf::IntRect({Xindex * 64, Yindex * 64 }, { 64, 64 }) );
+    playerSprite.scale(sf::Vector2f(2,2)); // increase size of shown image by 3
+    //--------------------------- LOAD ---------------------------
+    
+    
+    
+    
+    // main game loop
+    while (window.isOpen()) // run the program as long as the window is open
     {
         //--------------------------- UPDATE ---------------------------
-        // check all the window's events that were triggered since the last iteration of the loop
-        while (const std::optional event = window.pollEvent())
+        // EVENT LOOP
+        while (const std::optional event = window.pollEvent()) 
         {
-            // "close requested" event: we close the window
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
-        //--------------------------- UPDATE ---------------------------
-        
+
+        //OUTSIDE OF THE EVENT LOOF IS FASTER
+        //MOVEMENT
+        sf::Vector2f position = playerSprite.getPosition(); //get current position
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+             playerSprite.setPosition(position + sf::Vector2f(1, 0));
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+            playerSprite.setPosition(position + sf::Vector2f(0, -1));
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+            playerSprite.setPosition(position + sf::Vector2f(-1, 0));
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+            playerSprite.setPosition(position + sf::Vector2f(0, 1));
+
+        // --------------------------- UPDATE---------------------------
+                
         //--------------------------- DRAW ---------------------------
-        // clear the window with black color
-        window.clear(sf::Color::Black);
-        window.draw(shape);
-        window.draw(rectangle);
+        window.clear(sf::Color::Black); // clear the window with black color
+        window.draw(playerSprite);
         window.display();
         //--------------------------- DRAW ---------------------------
     }
